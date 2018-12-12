90af64cba135189b75a598f30c8bd4abb10a5eb6
cossu.lorenzo <lolloc123@gmail.com>
2018-12-12 10:17:59 +0100

    Aggiunta seconda imgView alla schermata principale

--- app/src/main/java/com/example/cossulorenzo/skynow/ViewPagerAdpapter.java
+++ app/src/main/java/com/example/cossulorenzo/skynow/ViewPagerAdpapter.java
@@ -14,7 +14,7 @@ public class ViewPagerAdpapter extends PagerAdapter {
 
     private Context context;
     private LayoutInflater layoutInflater;
-    private Integer [] images = {R.drawable.img1, R.drawable.img2};
+    private Integer [] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4 };
 
     public ViewPagerAdpapter(Context context) {
         this.context = context;
